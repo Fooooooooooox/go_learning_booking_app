2651efe60b5442bb6b745c39cf0535b3e3f32fe4
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-05-03 18:51:37 +0800

    iota learning （geth opcode push

---
+++ fancy_types_learning/fancy_types_test.go
@@ -0,0 +1,54 @@
+package fancy_types_test
+
+import (
+	"math/big"
+	"testing"
+)
+
+func TestBig(t *testing.T) {
+	var v1 int64
+	var v2 int64
+	v1 = 100
+	v2 = 101
+	a := big.NewInt(v1)
+	b := big.NewInt(v2)
+
+	// big.int是没有办法直接用> <比较大小的
+	// a>b返回1，a<b返回-1，a=b返回0
+	TeatIsValid1 := a.Cmp(b)
+	t.Log(TeatIsValid1)
+
+	// c := big.NewInt(1)
+
+}
+
+// iota最朴素的用法
+func TestIota(t *testing.T) {
+	const (
+		TestA = iota
+		TestB
+		TestC
+	)
+	t.Log(TestA)
+	t.Log(TestB)
+	t.Log(TestC)
+}
+
+// goeth core/vm/opcodes.go 里iota的用法
+
+func TestPrint(t *testing.T) {
+	type TestOpCode byte
+
+	const (
+		// 60的十六进制是96
+		PUSH1 TestOpCode = 0x60 + iota
+		PUSH2
+		PUSH3
+		PUSH4
+	)
+
+	t.Log(PUSH1)
+	t.Log(PUSH2)
+	t.Log(PUSH3)
+	t.Log(PUSH4)
+}
