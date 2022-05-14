006baf67be7c437239fd423c6b4377cfd0642690
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-05-14 22:48:47 +0800

    interface1 fix

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Fooooooooooox/go_learning_booking_app
 
 go 1.17
 
-require github.com/holiman/uint256 v1.2.0 // indirect
+require github.com/holiman/uint256 v1.2.0
--- interface_learning/interface_learning.go
+++ interface_learning/interface_learning.go
@@ -1,1 +1,41 @@
-package interface_learning2
+package interface_learning
+
+import (
+	"math"
+)
+
+type Shape interface {
+	area() float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+type Rect struct {
+	width  float64
+	height float64
+}
+
+func (r Rect) area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func GetArea(s Shape) float64 {
+	return s.area()
+}
+
+func GetAreas() []float64 {
+	c1 := Circle{4.5}
+	r1 := Rect{1, 2}
+	Shapes := []Shape{c1, r1}
+	var areas []float64
+	for _, shape := range Shapes {
+		areas = append(areas, GetArea(shape))
+	}
+	return areas
+}
