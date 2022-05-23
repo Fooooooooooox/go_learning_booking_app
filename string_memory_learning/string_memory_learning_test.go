ca7df22fcca2ced2c8514eee4eb29805480274c6
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-05-23 22:07:26 +0800

    string_memory

---
+++ string_memory_learning/string_memory_learning_test.go
@@ -0,0 +1,20 @@
+package stringmemorylearning_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+	"unsafe"
+)
+
+func TestJianyu(t *testing.T) {
+	s := "脑子进煎鱼了"
+	s1 := "脑子进煎鱼了"
+	s2 := "脑子进煎鱼了"[7:]
+
+	fmt.Printf("%d \n", (*reflect.StringHeader)(unsafe.Pointer(&s)).Data)
+	fmt.Printf("%d \n", (*reflect.StringHeader)(unsafe.Pointer(&s1)).Data)
+	fmt.Printf("%d \n", (*reflect.StringHeader)(unsafe.Pointer(&s2)).Data)
+}
+
+// go 中的内存管理 这三个string指向的内存都是一样的
