81fb2a53b0f57c8131b61a5ebc1badcf56698511
Fooooooooooox <crunchy.wafer.stick787@gmail.com>
2022-05-24 20:50:51 +0800

    operator bitwiseor

--- operator_learning/operator_learning_test.go
+++ operator_learning/operator_learning_test.go
@@ -20,4 +20,17 @@ func TestOperators(t *testing.T) {
 	}
 
 	// n << x is "n times 2, x times". And y >> z is "y divided by 2, z times".
+
+}
+
+// bitwiseor
+// |= 和 +=是一个意思
+// Like a += b is equivalent to a = a + b, a |= b is equivalent to a = a | b.
+
+func TestBitwiseor(t *testing.T) {
+	var x, y uint
+	x, y = 1, 0
+	y |= x
+	t.Log(x)
+	t.Log(y)
 }
